82708969bec5888f338cd7da2baa175bacc6a116
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-01-08 23:59:47 +0600

    feat: added method for task priority selection

--- src/model/TaskManager.java
+++ src/model/TaskManager.java
@@ -1,10 +1,13 @@
 package model;
 
+import enums.Priority;
+import services.IOManager;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
-    private List<Task> tasks;
+    private final List<Task> tasks;
 
     public TaskManager() {
         tasks = new ArrayList<Task>();
@@ -14,12 +17,21 @@ public class TaskManager {
         this.tasks = tasks;
     }
 
-    public void showAllTasks(){
-        if(tasks.isEmpty()){
+    public void showAllTasks() {
+        if (tasks.isEmpty()) {
             System.out.println("Список задач пуст");
             return;
         }
         System.out.println("Список всех задач: ");
         tasks.forEach(System.out::println);
     }
+    private String choicePriority() {
+        System.out.println("Выберите приоритет задачи: ");
+        for (Priority priority : Priority.values()) {
+            System.out.println(priority.ordinal() + 1 + ". " + priority.getValue());
+        }
+        int choice = Integer.parseInt(IOManager.getValidInput("^[1-3]$", "Введите число: "));
+        return Priority.values()[choice - 1].getValue();
+    }
+
 }
