e0322b071e80387ba134a5539b141756256be853
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-01-08 23:55:54 +0600

    feat: implement IOManager class for validated user input

---
+++ src/services/IOManager.java
@@ -0,0 +1,25 @@
+package services;
+
+import exceptions.IncorrectInputException;
+
+import java.util.Scanner;
+
+public class IOManager {
+    private final static Scanner sc = new Scanner(System.in);
+
+    public IOManager() {}
+
+    public static String getValidInput(String regex, String message){
+        try{
+            System.out.print(message);
+            String input = sc.nextLine().strip();
+            if(!input.matches(regex) || input.isBlank()){
+                throw new IncorrectInputException("Неверный ввод! Попробуйте еще раз!");
+            }
+            return input;
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+            return getValidInput(regex, message);
+        }
+    }
+}
