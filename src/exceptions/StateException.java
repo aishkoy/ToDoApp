70c94ff5f50132692190ddca535c1bedaa2dc584
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-01-08 18:09:14 +0600

    eat: added StateException class for custom exception handling
    - Created the StateException class in the `exceptions` package.
    - Includes constructors for default and detailed error messages.

---
+++ src/exceptions/StateException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class StateException extends Exception {
+    public StateException() {}
+    public StateException(String message) {
+        super(message);
+    }
+}
