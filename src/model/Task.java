29dae6fbc04f8250f1de1d9b51650fb8bbeaa5d3
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-01-09 18:20:31 +0600

    feat: getters were added

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,5 +1,6 @@
 package model;
 
+import enums.Priority;
 import enums.State;
 
 import java.time.LocalDate;
@@ -7,13 +8,13 @@ import java.time.LocalDate;
 public class Task {
     private final String name;
     private String description;
-    private final String priority;
+    private final Priority priority;
     private final LocalDate creationDate;
     private final LocalDate completionDate;
 
     private State state;
 
-    public Task(String name, String description, String priority, LocalDate completionDate, LocalDate creationDate) {
+    public Task(String name, String description, Priority priority, LocalDate completionDate, LocalDate creationDate) {
         this.name = name;
         this.description = description;
         this.completionDate = completionDate;
@@ -40,6 +41,18 @@ public class Task {
 
     public LocalDate getCompletionDate(){
         return completionDate;
+    }
+
+    public LocalDate getCreationDate(){
+        return creationDate;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public String getName() {
+        return name;
     }
 
     @Override
